dfd232ee9cac1939432c58d0bd706b7a74d6dd77
Chantal Goethals <chantalgoethals@gmail.com>
2022-06-08 21:28:23 -0600

    Feat: add event listeners

--- src/player.js
+++ src/player.js
@@ -1,8 +1,18 @@
-// class Player {
-//     constructor(){
+class Player {
+    constructor(){
+     this.name = name;
+     this.token = token;
+     this.win = true;
+    }
+    takeTurn(){
+    var 🛸 = this.token
+     if (this.win === true){
+       return this.token 
+    } else {
+        return this.token
+    }
+    console.log(this.token)
+ };
 
-//     }
-// };
 
-
-// module.exports = Player;
+module.exports = Player;
